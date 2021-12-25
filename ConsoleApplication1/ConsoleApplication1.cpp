bd50d8cf2228b58f425f0e2c292d2424a2512f83
пуке <aaa@mail.ru>
2021-12-25 11:44:01 +0700

    wret

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,33 @@
+﻿// Example program
+#include <iostream>
+
+bool check(char bad[256], char text[256]) {
+    int i = 0;
+    int j = 0;
+    while (text[i] != '\0') {
+        j = 0;
+        while (bad[j] != '\0') {
+            if (text[i] == bad[j])
+                return false;
+            j++;
+        }
+        i++;
+    }
+    return true;
+}
+
+int main()
+{
+    char bad[256];
+    char text[256];
+
+    std::cout << "INSERT TEXT ";
+    std::cin >> text;
+    std::cout << "INSERT MASK ";
+    std::cin >> bad;
+
+    if (check(bad, text))
+        std::cout << "TRUE";
+    else
+        std::cout << "FALSE";
+}
